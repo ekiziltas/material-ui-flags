72df8c4190e075fa27c10ff8b1659a88bbbe9625
ekiziltas <erryan1988@gmail.com>
2019-12-24 16:38:37 +0300

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@
  * Licensed under the MIT license.
  */
 
- export Icon_Flag_AU from './Icon_Flag_AU';
- export Icon_Flag_BG from './Icon_Flag_BG';
- export Icon_Flag_BR from './Icon_Flag_BR';
- export Icon_Flag_CA from './Icon_Flag_CA';
- export Icon_Flag_CN from './Icon_Flag_CN';
- export Icon_Flag_DE from './Icon_Flag_DE';
- export Icon_Flag_FR from './Icon_Flag_FR';
- export Icon_Flag_IN from './Icon_Flag_IN';
- export Icon_Flag_RU from './Icon_Flag_RU';
- export Icon_Flag_US from './Icon_Flag_US';
- export Icon_Flag_TR from './Icon_Flag_TR';
+ export IconFlagAU from './Icon_Flag_AU';
+ export IconFlagBG from './Icon_Flag_BG';
+ export IconFlagBR from './Icon_Flag_BR';
+ export IconFlagCA from './Icon_Flag_CA';
+ export IconFlagCN from './Icon_Flag_CN';
+ export IconFlagDE from './Icon_Flag_DE';
+ export IconFlagFR from './Icon_Flag_FR';
+ export IconFlagIN from './Icon_Flag_IN';
+ export IconFlagRU from './Icon_Flag_RU';
+ export IconFlagUS from './Icon_Flag_US';
+ export IconFlagTR from './Icon_Flag_TR';
