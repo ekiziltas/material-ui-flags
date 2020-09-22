a009844f1af48dad34022f48b9a1ab9c366cfd1d
ekiziltas <kiziltaseray@gmail.com>
2020-09-22 10:15:52 +0300

    CN Flag added

--- src/Icon_Flag_CN.js
+++ src/Icon_Flag_CN.js
@@ -6,18 +6,18 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 // SVG source:
 // https://github.com/lipis/flag-icon-css/tree/master/flags/4x3
 
-export default function( props )
-{
-  return(
-    <SvgIcon viewBox="0 0 640 480" {...props}>
-      <g transform="matrix(.48 0 0 .48 -2.6 2.39)">
-        <path fill="#de2910" d="M-5-15.4h30v20H-5z"/>
-        <use height="20" width="30" transform="matrix(3 0 0 3 0 -10.4)"/>
-        <use height="20" width="30" transform="rotate(-120.93 -1.29 -8.12)"/>
-        <use height="20" width="30" transform="rotate(-98.11 -1.45 -8.74)"/>
-        <use height="20" width="30" transform="rotate(-74.04 -2.07 -8.84)"/>
-        <use height="20" width="30" transform="rotate(-51.32 -4.16 -8.4)"/>
-      </g>
-    </SvgIcon>
+export default function (props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" width="640" height="480" viewBox="-5 -5 30 20">
+      <rect fill="#de2910" x="-5" y="-5" width="30" height="20"/>
+      <defs>
+        <polygon id="s" points="0,-513674 301930,415571 -488533,-158734 488533,-158734 -301930,415571" fill="#ffde00" transform="scale(0.0000019467600073)"/>
+      </defs>
+      <use xlinkHref="#s" transform="scale(3)"/>
+      <use xlinkHref="#s" transform="translate(5,-3) rotate(-120.963756)"/>
+      <use xlinkHref="#s" transform="translate(7,-1) rotate(-98.130102)"/>
+      <use xlinkHref="#s" transform="translate(7,2) rotate(-74.054604)"/>
+      <use xlinkHref="#s" transform="translate(5,4) rotate(-51.3401917)"/>
+    </svg>
   )
 }
---
+++ test/src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './App.css';
+import {
+  IconFlagTR,
+  IconFlagCN,
+  IconFlagES,
+  IconFlagDE,
+  IconFlagAU,
+  IconFlagBG,
+  IconFlagBR,
+  IconFlagCA,
+  IconFlagFR,
+  IconFlagIN,
+  IconFlagRU,
+  IconFlagUS
+} from 'material-ui-flags';
+
+class App extends Component {
+  render() {
+    return (
+      <div><IconFlagTR/></div>
+    );
+  }
+}
+
+export default App;
