05515a79ce94337fedfe0aa5d5273b7f427b0773
ekiziltas <kiziltaseray@gmail.com>
2020-09-26 02:26:58 +0300

    UK Flag Added

--- test/src/App.js
+++ test/src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
 import {
-  IconFlagTR
+  IconFlagUK
 } from 'material-ui-flags';
 
 class App extends Component {
   render() {
     return (
-      <div><IconFlagTR/></div>
+      <div><IconFlagUK/></div>
     );
   }
 }
